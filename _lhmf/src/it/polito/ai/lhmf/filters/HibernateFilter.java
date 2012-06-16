cc18a47b4577c7ce05be4dbd1210eb44362962a2
francesco.brundu@gmail.com <francesco.brundu@gmail.com@793d18dd-ad17-5a9a-07aa-97515492b075>
2012-06-16 19:48:51 +0000

    Refactored HibernateFilter using web.xml instead of annotations

--- _lhmf/src/it/polito/ai/lhmf/filters/HibernateFilter.java
+++ _lhmf/src/it/polito/ai/lhmf/filters/HibernateFilter.java
@@ -8,7 +8,6 @@ import javax.servlet.FilterConfig;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
-import javax.servlet.annotation.WebFilter;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
@@ -18,58 +17,73 @@ import org.hibernate.service.ServiceRegistry;
 import org.hibernate.service.ServiceRegistryBuilder;
 
 /**
- * Servlet Filter implementation class HibernteFilter
+ * Servlet Filter implementation class HibernateFilter
  */
-@WebFilter("/*")
-public class HibernteFilter implements Filter {
+public class HibernateFilter implements Filter
+{
 	private SessionFactory sf;
-    /**
-     * Default constructor. 
-     */
-    public HibernteFilter() {
-        // TODO Auto-generated constructor stub
-    }
+
+	/**
+	 * Default constructor.
+	 */
+	public HibernateFilter()
+	{
+		// TODO Auto-generated constructor stub
+	}
 
 	/**
 	 * @see Filter#destroy()
 	 */
-	public void destroy() {
+	public void destroy()
+	{
 		// TODO Auto-generated method stub
 	}
 
 	/**
 	 * @see Filter#doFilter(ServletRequest, ServletResponse, FilterChain)
 	 */
-	public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
+	public void doFilter(ServletRequest request, ServletResponse response,
+			FilterChain chain) throws IOException, ServletException
+	{
 		Session hibernateSession = sf.getCurrentSession();
 		request.setAttribute("hibernate_session", hibernateSession);
-		Transaction tx=null;
-	    try {
-	    	tx=hibernateSession.beginTransaction();
-	    	chain.doFilter(request, response);
-	    	tx.commit();
-	    } catch (Throwable ex) {
-	    	if (tx!=null) 
-	    		tx.rollback();
-	    	throw new ServletException(ex);
-	    } finally {
-	    	if(hibernateSession != null && hibernateSession.isOpen())
-	    		hibernateSession.close();
-	    }
+		Transaction tx = null;
+		try
+		{
+			tx = hibernateSession.beginTransaction();
+			chain.doFilter(request, response);
+			tx.commit();
+		}
+		catch (Throwable ex)
+		{
+			if (tx != null)
+				tx.rollback();
+			throw new ServletException(ex);
+		}
+		finally
+		{
+			if (hibernateSession != null && hibernateSession.isOpen())
+				hibernateSession.close();
+		}
 
 	}
 
 	/**
 	 * @see Filter#init(FilterConfig)
 	 */
-	public void init(FilterConfig fConfig) throws ServletException {
-		try {
+	public void init(FilterConfig fConfig) throws ServletException
+	{
+		try
+		{
 			Configuration configuration = new Configuration();
 			configuration.configure();
-			ServiceRegistry serviceRegistry = new ServiceRegistryBuilder().applySettings(
-			configuration.getProperties()).buildServiceRegistry();
+			ServiceRegistry serviceRegistry = new ServiceRegistryBuilder()
+					.applySettings(configuration.getProperties())
+					.buildServiceRegistry();
 			sf = configuration.buildSessionFactory(serviceRegistry);
-		} catch (Throwable ex) {
+		}
+		catch (Throwable ex)
+		{
 			System.err.println("Failed to create sessionFactory object." + ex);
 			throw new ExceptionInInitializerError(ex);
 		}
