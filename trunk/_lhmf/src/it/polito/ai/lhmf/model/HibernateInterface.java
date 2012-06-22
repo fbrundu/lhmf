f6cf798d9d7c0b062410eaee960069a5baa35b34
francesco.brundu@gmail.com <francesco.brundu@gmail.com@793d18dd-ad17-5a9a-07aa-97515492b075>
2012-06-22 14:47:58 +0000

    CRUD completed for ProductCategory

--- trunk/_lhmf/src/it/polito/ai/lhmf/model/HibernateInterface.java
+++ trunk/_lhmf/src/it/polito/ai/lhmf/model/HibernateInterface.java
@@ -56,6 +56,26 @@ public abstract class HibernateInterface
 		return hibernateSession.createQuery("from ProductCategory").list();
 	}
 
+	public static Integer updateProductCategory(Session hibernateSession,
+			ProductCategory productCategory)
+			throws NoHibernateSessionException, InvalidParametersException
+	{
+		if (hibernateSession == null)
+			throw new NoHibernateSessionException();
+
+		if (productCategory == null)
+			throw new InvalidParametersException();
+
+		Query query = hibernateSession
+				.createQuery("update ProductCategory set description = :description"
+						+ " where idProductCategory = :idProductCategory");
+		query.setParameter("description", productCategory.getDescription());
+		query.setParameter("idProductCategory",
+				productCategory.getIdProductCategory());
+
+		return (Integer) query.executeUpdate();
+	}
+
 	public static Integer deleteProductCategory(Session hibernateSession,
 			Integer idProductCategory) throws NoHibernateSessionException,
 			InvalidParametersException
