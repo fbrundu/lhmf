652bc9d3bc806642ecb69b21e891009865e17b55
francesco.brundu@gmail.com <francesco.brundu@gmail.com@793d18dd-ad17-5a9a-07aa-97515492b075>
2012-06-23 19:50:16 +0000

    Uploading a minimal version of lib_supplier.js;

---
+++ trunk/_lhmf/WebContent/js/lib_supplier.js
@@ -0,0 +1,61 @@
+// HelloCategory: example on how to do ajax operations
+function HelloCategory()
+{
+  $(function()
+          {
+            var productCategory = new Object();
+            productCategory.idProductCategory = "1";
+            productCategory.description = "Descrizione2";
+            newCategory(productCategory);
+            productCategory.description = "Descrizione1";
+            setTimeout(function()
+            {
+              updateCategory(productCategory);
+            }, 3000);
+            setTimeout(function()
+            {
+              getCategories();
+            }, 5000);
+            setTimeout(function()
+            {
+              deleteCategory(productCategory.idProductCategory);
+            }, 7000);
+          });
+}
+
+// CRUD on ProductCategory
+function newCategory(productCategory)
+{
+  $.postJSON("ajax/newproductcategory", productCategory, function(
+      idProductCategory)
+  {
+    console.debug("Inserted: " + idProductCategory);
+  });
+}
+
+function updateCategory(productCategory)
+{
+  $.postJSON("ajax/updateproductcategory", productCategory, function(
+      rowsAffected)
+  {
+    console.debug("Updated: " + rowsAffected);
+  });
+}
+
+function getCategories()
+{
+  $.getJSON("ajax/getproductcategories", function(productCategoriesList)
+  {
+    window.localStorage.setItem('productCategoriesList', productCategoriesList);
+    //console.debug(productCategoriesList);
+  });
+}
+
+function deleteCategory(idProductCategory)
+{
+  $.postJSON("ajax/deleteproductcategory", idProductCategory, function(
+      rowsAffected)
+  {
+    console.debug("Deleted: " + rowsAffected);
+  });
+}
