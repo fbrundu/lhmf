a883138c812fdc2f3751b2fca76fdc093420da08
francesco.brundu@gmail.com <francesco.brundu@gmail.com@793d18dd-ad17-5a9a-07aa-97515492b075>
2012-07-13 16:00:55 +0000

    Added CRUD+Ajax controller+Hibernate interface for Member; need testing

---
+++ _lhmf/src/it/polito/ai/lhmf/model/MemberInterface.java
@@ -0,0 +1,102 @@
+package it.polito.ai.lhmf.model;
+
+import it.polito.ai.lhmf.exceptions.InvalidParametersException;
+import it.polito.ai.lhmf.orm.Member;
+
+import java.util.List;
+
+import org.hibernate.Query;
+import org.hibernate.SessionFactory;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
+
+public class MemberInterface
+{
+	// The session factory will be automatically injected by spring
+	private SessionFactory sessionFactory;
+
+	public void setSessionFactory(SessionFactory sf)
+	{
+		this.sessionFactory = sf;
+	}
+
+	@Transactional(propagation = Propagation.REQUIRED)
+	public Integer newMember(Member member) throws InvalidParametersException
+	{
+		if (member == null)
+			throw new InvalidParametersException();
+
+		return (Integer) sessionFactory.getCurrentSession().save(member);
+	}
+
+	@Transactional(readOnly = true)
+	public Member getMember(Integer idMember)
+	{
+		Query query = sessionFactory.getCurrentSession().createQuery(
+				"from Member" + "where idMember = :idMember");
+		query.setParameter("idMember", idMember);
+		return (Member) query.uniqueResult();
+	}
+
+	@SuppressWarnings("unchecked")
+	@Transactional(readOnly = true)
+	public List<Member> getMembers()
+	{
+		return sessionFactory.getCurrentSession().createQuery("from Member")
+				.list();
+	}
+
+	@Transactional(propagation = Propagation.REQUIRED)
+	public Integer updateMember(Member member)
+			throws InvalidParametersException
+	{
+		if (member == null)
+			throw new InvalidParametersException();
+
+		Query query = sessionFactory.getCurrentSession().createQuery(
+				"update Member"
+						+ "set name = :name,"
+						+ "surname = :surname,"
+						+ "username = :username"
+						// + "password = :password"
+						// + "regCode = :regCode" + "regDate = :regDate"
+						+ "email = :email" + "address = :address"
+						+ "city = :city" + "state = :state" + "cap = :cap"
+						+ "tel = :tel" + "memberType = :memberType"
+						+ "status = :status" + "where idMember = :idMember");
+		query.setParameter("name", member.getName());
+		query.setParameter("surname", member.getSurname());
+		query.setParameter("username", member.getUsername());
+		// query.setParameter("password", member.getPassword());
+		// query.setParameter("regCode", member.getRegCode());
+		// query.setParameter("regDate", member.getRegDate());
+		query.setParameter("email", member.getEmail());
+		query.setParameter("address", member.getAddress());
+		query.setParameter("city", member.getCity());
+		query.setParameter("state", member.getState());
+		query.setParameter("cap", member.getCap());
+		query.setParameter("tel", member.getTel());
+		query.setParameter("memberType", member.getMemberType()
+				.getIdMemberType());
+		query.setParameter("status", member.getMemberStatus()
+				.getIdMemberStatus());
+		query.setParameter("idMember", member.getIdMember());
+
+		return (Integer) query.executeUpdate();
+	}
+
+	@Transactional(propagation = Propagation.REQUIRED)
+	public Integer deleteMember(Integer idMember)
+			throws InvalidParametersException
+	{
+		if (idMember == null)
+			throw new InvalidParametersException();
+
+		Query query = sessionFactory.getCurrentSession().createQuery(
+				"delete from Member" + "where idMember = :idMember");
+
+		query.setParameter("idMember", idMember);
+
+		return (Integer) query.executeUpdate();
+	}
+}
