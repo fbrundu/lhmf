0da9563272927641139a75e6ddff9b84283c0cba
hasmet87@gmail.com <hasmet87@gmail.com@793d18dd-ad17-5a9a-07aa-97515492b075>
2012-07-15 09:06:40 +0000

    CRUD completato su purchase

--- trunk/_lhmf/src/it/polito/ai/lhmf/model/PurchaseInterface.java
+++ trunk/_lhmf/src/it/polito/ai/lhmf/model/PurchaseInterface.java
@@ -1,10 +1,13 @@
 package it.polito.ai.lhmf.model;
 
+import java.util.Calendar;
 import java.util.List;
 
 import it.polito.ai.lhmf.exceptions.InvalidParametersException;
+import it.polito.ai.lhmf.orm.Order;
 import it.polito.ai.lhmf.orm.Purchase;
 
+import org.hibernate.Query;
 import org.hibernate.SessionFactory;
 import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
@@ -34,19 +37,19 @@ public class PurchaseInterface
 	@Transactional(readOnly=true)
 	public List<Purchase> getPastPurchase()
 	{ 
-		//Query query = sessionFactory.getCurrentSession().createQuery("from Order" + "where date_close <= :date_close"); 
-		//String dateToQuery = new String(Calendar.YEAR+""+Calendar.MONTH+""+Calendar.DAY_OF_MONTH);
-		//query.setParameter("date_close", dateToQuery);
-		//return query.list();
-		return sessionFactory.getCurrentSession().createQuery("from Purchase").list();
+		Query queryOrders = sessionFactory.getCurrentSession().createQuery("from Order" + "where date_close <= :date_close"); 
+		String dateToQuery = new String(Calendar.YEAR+""+Calendar.MONTH+""+Calendar.DAY_OF_MONTH);
+		List<Order> orders = queryOrders.setParameter("date_close", dateToQuery).list();
+		Query queryPurchase = sessionFactory.getCurrentSession().createQuery("from Purchase" + "where idPurchase = :idPurchase");
+		return queryPurchase.setParameterList("idPurchase", orders).list();
 	}
 	
 	@SuppressWarnings("unchecked")
 	@Transactional(readOnly=true)
 	public List<Purchase> getActivePurchase()
 	{
-		return sessionFactory.getCurrentSession().createQuery("from Purchase").list();
-	}
-	
-	
+		List<Order> orders = sessionFactory.getCurrentSession().createQuery("from Order" + "where date_close = 0").list();
+		Query queryPurchase = sessionFactory.getCurrentSession().createQuery("from Purchase" + "where idPurchase = :idPurchase");
+		return queryPurchase.setParameterList("idPurchase", orders).list();
+	}	
 }
