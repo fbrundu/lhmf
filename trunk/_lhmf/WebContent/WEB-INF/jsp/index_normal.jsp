6aa11fa3a17a6d1c4af2ab8da3355327353627eb
hasmet87@gmail.com <hasmet87@gmail.com@793d18dd-ad17-5a9a-07aa-97515492b075>
2012-07-25 13:14:22 +0000

    novità utente

--- trunk/_lhmf/WebContent/WEB-INF/jsp/index_normal.jsp
+++ trunk/_lhmf/WebContent/WEB-INF/jsp/index_normal.jsp
@@ -1,26 +1,28 @@
 <%@ page language="java" contentType="text/html; charset=UTF-8"
 	pageEncoding="UTF-8"%>
 <%@taglib prefix="t" tagdir="/WEB-INF/tags"%>
+<%@ taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<%@ taglib prefix="spring" uri="http://www.springframework.org/tags"%>
 
 <t:index>
-  <jsp:attribute name="userMenu">
-      <div class="round-border-topright"></div>
-		<h1 class="first">Menu Utente</h1>
-		<dl class="menu-navigazione">
-		  <dt><a href="">Crea Scheda</a></dt>
-		  <dt><a href="">Visualizza Schede</a></dt>
-		  <dt><a href="">Notifiche (NN)</a></dt>
-	      <dt><a href="">Messaggi (NN)</a></dt>				
-		</dl> 
-    </jsp:attribute>
-    
-    <jsp:attribute name="scripts">
+
+	<jsp:attribute name="scripts">
 		<script type="text/javascript" src='<spring:url htmlEscape="true" value="/js/lib_normal.js"/>'></script> 
 		<script>
 	  	function drawPageCallback(){
 			writeIndexPage();
 		}
 	  	</script>
+    </jsp:attribute>
+
+  	<jsp:attribute name="userMenu">
+      <div class="round-border-topright"></div>
+		<h1 class="first">Menu Utente</h1>
+		<dl class="menu-navigazione">
+		  <dt><a id="purchaseLink">Schede Di Acquisto</a></dt>
+		  <dt><a href="">Notifiche (NN)</a></dt>
+	      <dt><a href="">Messaggi (NN)</a></dt>				
+		</dl> 
     </jsp:attribute>
     
   	<jsp:attribute name="bodyTitle">Interfaccia Utente Normale</jsp:attribute>
--- trunk/_lhmf/WebContent/js/lib_normal.js
+++ trunk/_lhmf/WebContent/js/lib_normal.js
@@ -1,4 +1,170 @@
-//////
+(function(window, undefined) {
+    var History = window.History;
+    $ = window.jQuery;
+    var histEnabled = History.enabled;
+    if (!histEnabled)
+        console.log("HTML 5 History API is disabled!");
+    else
+        History.Adapter.bind(window, 'statechange', historyStateChanged);
+
+    $(function() 
+    {
+        $("#purchaseLink").click(purchaseClicked);
+
+        $.datepicker.setDefaults({
+            dateFormat : 'dd/mm/yy'
+        });
+        drawPageCallback();
+    });
+
+    function historyStateChanged()
+    {
+        var History = window.History;
+        var state = History.getState();
+        var stateData = state.data;
+        if (!stateData)
+            showIndex();
+        switch (stateData.action)
+        {
+        case 'purchase':
+            writePurchasePage();
+            break;
+        default:
+            writeIndexPage();
+        }
+    }
+
+    function purchaseClicked(event) {
+        if (histEnabled == true) {
+            event.preventDefault();
+            var History = window.History;
+            var state = History.getState();
+            var stateData = state.data;
+            if (!!stateData && !!stateData.action && stateData.action == 'purchase')
+                return;
+            History.pushState({action : 'purchase'}, null, 'purchase');
+        }
+    }
+    
+    function writePurchasePage(){
+        $(".centrale").html("<div id='tabsPurchase'>" +
+        		                    "<ul>" +
+        		                     "<li><a href='#tabsPurchase-1'>Crea Scheda</a></li>" +
+        		                     "<li><a href='#tabsPurchase-2'>Schede Attive</a></li>" +
+        		                     "<li><a href='#tabsPurchase-3'>Schede Scadute</a></li>" +
+        		                     "<li><a href='#tabsPurchase-4'>Schede In Consegna</a></li>" +
+        		                    "</ul>" +
+                                    "<div id='tabsPurchase-1'></div>" +
+                                    "<div id='tabsPurchase-2'></div>" +
+                                    "<div id='tabsPurchase-3'></div>" +
+                                    "<div id='tabsPurchase-4'></div>" +
+                               "</div>");
+        
+        $('#tabsPurchase-1').html("<div class='logform'>" +
+                                "<form method='post' action='prder'>" +
+                                  "<fieldset><legend>&nbsp;Composizione della Nuova Scheda:&nbsp;</legend><br />" +
+                                    "drag and drop" +
+                                    "<button type='submit' id='orderRequest'> Crea Scheda </button>" +
+                                  "</fieldset>" +
+                                  "<div id='errorDivPurchase' style='display:none;'>" +
+                                      "<fieldset><legend id='legendErrorPurchase'>&nbsp;Errore&nbsp;</legend><br />" +
+                                       "<div id='errorsPurchase' style='padding-left: 40px'></div>" +
+                                      "</fieldset>" +
+                                  "</div>" +
+                                "</form>" +
+                              "</div>");
+        
+        $('#tabsPurchase-2').html("<div class='logform'>" +
+                                "<form method='post' action=''>" +
+                                  "<fieldset><legend>&nbsp;Opzioni di Ricerca Schede Attive:&nbsp;</legend><br />" +
+                                      "<label for='minDate' class='left'>Data iniziale: </label>" +
+                                      "<input type='text' id='minDate' class='field'/>" +
+                                      "<label for='maxDate' class='left'>Data finale: </label>" +
+                                      "<input type='text' id='maxDate' class='field'/>" +
+                                  "</fieldset>" +
+                                  "<button type='submit' id='purchaseActiveRequest'> Visualizza </button>" +
+                                "</form>" +
+                                "<table id='activePurchaseList' class='log'></table>" +
+                                  "<div id='errorDivActivePurchase' style='display:none;'>" +
+                                    "<fieldset><legend id='legendErrorActivePurchase'>&nbsp;Errore&nbsp;</legend><br />" +
+                                     "<div id='errorsActivePurchase' style='padding-left: 40px'>" +
+                                      "</div>" +
+                                    "</fieldset>" +
+                                  "</div><br />" +
+                              "</div>");
+        
+        $('#tabsPurchase-3').html("<div class='logform'>" +
+                                "<form method='post' action=''>" +
+                                  "<fieldset><legend>&nbsp;Opzioni di Ricerca Schede Scadute:&nbsp;</legend><br />" +
+                                      "<label for='minDate2' class='left'>Data iniziale: </label>" +
+                                      "<input type='text' id='minDate2' class='field'/>" +
+                                      "<label for='maxDate2' class='left'>Data finale: </label>" +
+                                      "<input type='text' id='maxDate2' class='field'/>" +
+                                      "<br /><label for='toSetShipDate' class='left'>Ordini con Data di Consegna da impostare: </label>" +
+                                      "<input type='checkbox' id='toSetShipDate' />" +
+                                  "</fieldset>" +
+                                  "<button type='submit' id='orderOldRequest'> Visualizza </button>" +
+                                "</form>" +
+                                "<table id='oldPurchaseList' class='log'></table>" +
+                                  "<div id='errorDivOldPurchase' style='display:none;'>" +
+                                    "<fieldset><legend id='legendErrorOldPurchase'>&nbsp;Errore&nbsp;</legend><br />" +
+                                     "<div id='errorsOldPurchase' style='padding-left: 40px'>" +
+                                      "</div>" +
+                                    "</fieldset>" +
+                                  "</div><br />" +
+                              "</div>");
+        
+        $('#tabsPurchase-4').html("Schede in fase di Consegna");
+       
+        preparePurchaseForm();
+    }
+
+    function writeIndexPage()
+    {
+        $('.centrale').html("<p>Interfaccia utente normale</p>");
+    }
+    
+})(window);
+
+function clickPurchaseHandler(event) {
+    event.preventDefault();
+  
+    
+}
+
+function clickPurchaseActiveHandler(event) {
+    event.preventDefault();
+    
+    //Schede attive
+  
+    
+}
+
+function clickPurchaseOldHandler(event) {
+    event.preventDefault();
+  
+    //Schede passate
+    
+}
+
+function preparePurchaseForm(tab){
+    
+    $('#tabsPurchase').tabs();
+    
+    $("#minDate").datepicker({ defaultDate: 0, maxDate: 0 });
+    $('#minDate').datepicker("setDate", Date.now());
+    $('#maxDate').datepicker({ defaultDate: 0, maxDate: 0 });
+    $('#maxDate').datepicker("setDate", Date.now());
+    $("#minDate2").datepicker({ defaultDate: 0, maxDate: 0 });
+    $('#minDate2').datepicker("setDate", Date.now());
+    $('#maxDate2').datepicker({ defaultDate: 0, maxDate: 0 });
+    $('#maxDate2').datepicker("setDate", Date.now());
+    
+    $('#purchaseRequest').on("click", clickPurchaseHandler);
+    $('#purchaseActiveRequest').on("click", clickPurchaseActiveHandler);
+    $('#purchaseOldRequest').on("click", clickPurchaseOldHandler);
+}
+
 function newPurchase(purchase)
 {
 	if (purchase == undefined)
