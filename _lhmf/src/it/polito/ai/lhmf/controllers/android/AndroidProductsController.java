7334bbd3064e7766943207b772b18456856d60d0
l.moretto88@gmail.com <l.moretto88@gmail.com@793d18dd-ad17-5a9a-07aa-97515492b075>
2012-08-03 10:41:52 +0000

    creazione prodotti da android terminata. L'utente, se vuole, può scegliere un immagina dalla galleria oppure fare una foto.

--- _lhmf/src/it/polito/ai/lhmf/controllers/android/AndroidProductsController.java
+++ _lhmf/src/it/polito/ai/lhmf/controllers/android/AndroidProductsController.java
@@ -1,16 +1,5 @@
 package it.polito.ai.lhmf.controllers.android;
 
-import java.io.BufferedOutputStream;
-import java.io.File;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.OutputStream;
-import java.security.Principal;
-import java.util.List;
-
-import javax.servlet.ServletContext;
-import javax.servlet.http.HttpServletRequest;
-
 import it.polito.ai.lhmf.exceptions.InvalidParametersException;
 import it.polito.ai.lhmf.model.MemberInterface;
 import it.polito.ai.lhmf.model.ProductCategoryInterface;
@@ -20,6 +9,12 @@ import it.polito.ai.lhmf.orm.Product;
 import it.polito.ai.lhmf.orm.ProductCategory;
 import it.polito.ai.lhmf.orm.Supplier;
 import it.polito.ai.lhmf.security.MyUserDetailsService;
+
+import java.security.Principal;
+import java.util.List;
+
+import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.access.prepost.PreAuthorize;
@@ -79,8 +74,7 @@ public class AndroidProductsController {
 			@RequestParam(value = "unitCost", required = true) float unitCost,
 			@RequestParam(value = "minBuy", required = true) int minBuy,
 			@RequestParam(value = "maxBuy", required = true) int maxBuy,
-			@RequestParam(value = "productCategory", required = true) int idProductCategory,
-			@RequestParam(value = "picture", required=false) MultipartFile picture )
+			@RequestParam(value = "productCategory", required = true) int idProductCategory )
 			throws InvalidParametersException {
 		Integer idProduct = -1;
 		Supplier s = supplierInterface.getSupplier(principal.getName());
@@ -105,14 +99,58 @@ public class AndroidProductsController {
 			p.setAvailability(true);
 			p.setSupplier(s);
 			p.setProductCategory(pc);
-			if(picture != null){
-				ServletContext context = request.getServletContext();
-				String path = context.getRealPath("/img/prd/");
-				
-				idProduct = productInterface.newProduct(p, picture, path);
-			}
-			else
-				idProduct = productInterface.newProduct(p);
+			idProduct = productInterface.newProduct(p);
+		}
+		return idProduct;
+	}
+	
+	@PreAuthorize("hasRole('" + MyUserDetailsService.UserRoles.SUPPLIER + "')")
+	@RequestMapping(value = "/androidApi/newproductwithpicture", method = RequestMethod.POST)
+	public @ResponseBody
+	Integer newProductWithPicture(
+			HttpServletRequest request,
+			Principal principal,
+			@RequestParam(value = "productName", required = true) String productName,
+			@RequestParam(value = "productDescription", required = true) String productDescription,
+			@RequestParam(value = "productDimension", required = true) int productDimension,
+			@RequestParam(value = "measureUnit", required = true) String measureUnit,
+			@RequestParam(value = "unitBlock", required = true) int unitBlock,
+			@RequestParam(value = "transportCost", required = true) float transportCost,
+			@RequestParam(value = "unitCost", required = true) float unitCost,
+			@RequestParam(value = "minBuy", required = true) int minBuy,
+			@RequestParam(value = "maxBuy", required = true) int maxBuy,
+			@RequestParam(value = "productCategory", required = true) int idProductCategory,
+			@RequestParam(value = "picture", required=true) MultipartFile picture )
+			throws InvalidParametersException {
+		Integer idProduct = -1;
+		Supplier s = supplierInterface.getSupplier(principal.getName());
+		ProductCategory pc = productCategoryInterface
+				.getProductCategory(idProductCategory);
+		if (s != null && pc != null && !productName.equals("")
+				&& !productDescription.equals("") && productDimension > 0
+				&& !measureUnit.equals("") && unitBlock > 0
+				&& transportCost > 0 && unitCost > 0 && minBuy > 0
+				&& maxBuy >= minBuy)
+		{
+			Product p = new Product();
+			p.setName(productName);
+			p.setDescription(productDescription);
+			p.setDimension(productDimension);
+			p.setMeasureUnit(measureUnit);
+			p.setUnitBlock(unitBlock);
+			p.setTransportCost(transportCost);
+			p.setUnitCost(unitCost);
+			p.setMinBuy(minBuy);
+			p.setMaxBuy(maxBuy);
+			p.setAvailability(true);
+			p.setSupplier(s);
+			p.setProductCategory(pc);
+			
+			ServletContext context = request.getServletContext();
+			String serverPath = "img/prd/";
+			String realPath = context.getRealPath(serverPath);
+			
+			idProduct = productInterface.newProduct(p, picture, serverPath, realPath);
 		}
 		return idProduct;
 	}
