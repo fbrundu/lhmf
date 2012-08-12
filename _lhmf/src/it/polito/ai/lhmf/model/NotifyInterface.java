911a7cf794b83e61b7be45b69fd11ab1556936ee
francesco.brundu@gmail.com <francesco.brundu@gmail.com@793d18dd-ad17-5a9a-07aa-97515492b075>
2012-08-12 17:17:52 +0000

    Aggiunte funzioni NotifyInterface + aggiunto stub NotifyAjaxController

--- _lhmf/src/it/polito/ai/lhmf/model/NotifyInterface.java
+++ _lhmf/src/it/polito/ai/lhmf/model/NotifyInterface.java
@@ -1,7 +1,6 @@
 package it.polito.ai.lhmf.model;
 
 import it.polito.ai.lhmf.exceptions.InvalidParametersException;
-import it.polito.ai.lhmf.orm.Message;
 import it.polito.ai.lhmf.orm.Notify;
 
 import java.util.List;
@@ -22,8 +21,7 @@ public class NotifyInterface
 	}
 
 	@Transactional(propagation = Propagation.REQUIRED)
-	public Integer newNotify(Notify notify)
-			throws InvalidParametersException
+	public Integer newNotify(Notify notify) throws InvalidParametersException
 	{
 		if (notify == null)
 			throw new InvalidParametersException();
@@ -42,10 +40,50 @@ public class NotifyInterface
 
 	@SuppressWarnings("unchecked")
 	@Transactional(readOnly = true)
-	public List<Message> getNotifies()
+	public List<Notify> getNotifiesByIdMember(Integer idMember)
+			throws InvalidParametersException
 	{
-		return sessionFactory.getCurrentSession().createQuery("from Notify")
-				.list();
+		if (idMember == null)
+			throw new InvalidParametersException();
+
+		Query query = sessionFactory.getCurrentSession().createQuery(
+				"from Notify " + "where idMember = :idMember");
+		query.setParameter("idMember", idMember);
+		return query.list();
+	}
+
+	@SuppressWarnings("unchecked")
+	@Transactional(readOnly = true)
+	public List<Notify> getNotifiesByIdMemberAfter(Integer idMember,
+			Integer idNotify) throws InvalidParametersException
+	{
+		if (idMember == null || idNotify == null)
+			throw new InvalidParametersException();
+
+		// FIXME : testare
+		Query query = sessionFactory.getCurrentSession().createQuery(
+				"from Notify " + "where idMember = :idMember"
+						+ "and idNotify > :idNotify");
+		query.setParameter("idMember", idMember);
+		query.setParameter("idNotify", idNotify);
+
+		return query.list();
+	}
+
+	@Transactional(propagation = Propagation.REQUIRED)
+	public Integer setReadBefore(Integer idNotify)
+			throws InvalidParametersException
+	{
+		if (idNotify == null)
+			throw new InvalidParametersException();
+
+		// FIXME : testare
+		Query query = sessionFactory.getCurrentSession().createQuery(
+				"update Notify" + " set isReaded = true"
+						+ " where idNotify <= :idNotify and isReaded = false");
+		query.setParameter("idNotify", idNotify);
+
+		return (Integer) query.executeUpdate();
 	}
 
 	@Transactional(propagation = Propagation.REQUIRED)
@@ -57,7 +95,6 @@ public class NotifyInterface
 
 		Query query = sessionFactory.getCurrentSession().createQuery(
 				"delete from Notify" + " where idNotify = :idNotify");
-
 		query.setParameter("idNotify", idNotify);
 
 		return (Integer) query.executeUpdate();
