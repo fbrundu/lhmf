9e714507549fbdbdc93c1f17361c276b36f1dadc
l.moretto88@gmail.com <l.moretto88@gmail.com@793d18dd-ad17-5a9a-07aa-97515492b075>
2012-10-04 16:37:14 +0000

    implementare visualizzazione schede dell'ordine e consegna singole schede (vedere FIXME in ShipOrderPurchasesActivity.java)

---
+++ trunk/AndroidGAS/src/it/polito/ai/lhmf/android/resp/ShipOrderPurchasesActivity.java
@@ -0,0 +1,10 @@
+package it.polito.ai.lhmf.android.resp;
+
+import android.app.Activity;
+
+
+//FIXME implementare visualizzazione schede dell'ordine, con info su quelle fallite perchè hanno tutti prodotti falliti,
+//+ implementare impostazione scheda consegnata
+public class ShipOrderPurchasesActivity extends Activity {
+
+}
---
+++ trunk/AndroidGAS/src/it/polito/ai/lhmf/android/resp/ShippedOrdersActivity.java
@@ -0,0 +1,163 @@
+package it.polito.ai.lhmf.android.resp;
+
+import it.polito.ai.lhmf.android.R;
+import it.polito.ai.lhmf.android.api.Gas;
+import it.polito.ai.lhmf.android.api.util.GasConnectionHolder;
+import it.polito.ai.lhmf.model.Order;
+
+import org.springframework.social.connect.Connection;
+
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.text.format.DateFormat;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
+import android.widget.TextView;
+
+
+public class ShippedOrdersActivity extends Activity {
+	private Gas api = null;
+	private CustomAdapter adapter = null;
+	
+	private ListView orderListView = null;
+	
+	private TextView noOrders = null;
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		
+		GasConnectionHolder holder = new GasConnectionHolder(getApplicationContext());
+		Connection<Gas> conn = holder.getConnection();
+		if(conn != null){
+			api  = conn.getApi();
+			setContentView(R.layout.active_orders);
+			
+			TextView title = (TextView) findViewById(R.id.title);
+			title.setText(R.string.shipped_orders);
+			
+			orderListView = (ListView) findViewById(R.id.ordersList);
+			
+			noOrders = (TextView) findViewById(R.id.no_orders_available);
+			noOrders.setText("Non ci sono ordini da visualizzare");
+		}
+	}
+	
+	@Override
+	protected void onResume() {
+		if(api != null){
+			new GetShippedOrdersTask().execute(api);
+		}
+		super.onResume();
+	}
+	
+	private class GetShippedOrdersTask extends AsyncTask<Gas, Void, Order[]>{
+
+		@Override
+		protected Order[] doInBackground(Gas... params) {
+			Gas gas = params[0];
+			
+			return gas.orderOperations().getRespShippedOrders();
+		}
+		
+		@Override
+		protected void onPostExecute(Order[] result) {
+			if(result != null && result.length > 0){
+				adapter = new CustomAdapter(ShippedOrdersActivity.this, R.layout.order_available_item, R.id.orderName, result);
+				orderListView.setAdapter(adapter);
+				orderListView.setVisibility(View.VISIBLE);
+				noOrders.setVisibility(View.GONE);
+			}
+			else{
+				orderListView.setVisibility(View.GONE);
+				noOrders.setVisibility(View.VISIBLE);
+			}
+		}		
+	}
+	
+	private class CustomAdapter extends ArrayAdapter<Order>{
+		
+		public CustomAdapter(Context context, int resource,
+				int textViewResourceId, Order[] objects) {
+			super(context, resource, textViewResourceId, objects);
+		}
+		
+		public CustomAdapter(Context context, int resource, int textViewResourceId) {
+			super(context, resource, textViewResourceId);
+		}
+
+		@Override
+		public View getView(int position, View convertView, ViewGroup parent) {
+			View row = convertView;
+			TextView orderName;
+			TextView orderSupplier;
+			TextView orderResp;
+			TextView respLabel;
+			TextView orderOpenDate;
+			TextView orderCloseDate;
+			TextView orderProgressLabel;
+			View orderProgressLayout;
+			Button orderDetails;
+			
+			TextView orderDeliveryLabel;
+			TextView orderDeliveryDate;
+			
+			final Order order = getItem(position);
+			
+			if(row == null){
+				LayoutInflater inflater = getLayoutInflater();
+				row = inflater.inflate(R.layout.order_available_item, parent, false);
+			}
+			orderName = (TextView) row.findViewById(R.id.orderName);
+			orderSupplier = (TextView) row.findViewById(R.id.orderSupplier);
+			
+			orderResp = (TextView) row.findViewById(R.id.orderResp);
+			orderResp.setVisibility(View.GONE);
+			respLabel = (TextView) row.findViewById(R.id.respLabel);
+			respLabel.setVisibility(View.GONE);
+			
+			orderOpenDate = (TextView) row.findViewById(R.id.orderOpenDate);
+			orderCloseDate = (TextView) row.findViewById(R.id.orderCloseDate);
+			
+			orderProgressLabel = (TextView) row.findViewById(R.id.order_progress_label);
+			orderProgressLabel.setVisibility(View.GONE);
+			
+			orderProgressLayout = row.findViewById(R.id.orderProgressLayout);
+			orderProgressLayout.setVisibility(View.GONE);
+			
+			orderDetails = (Button) row.findViewById(R.id.order_purchase_button);
+			
+			//Visualizzazione data di consegna
+			orderDeliveryLabel = (TextView) row.findViewById(R.id.delivery_date_label);
+			orderDeliveryLabel.setVisibility(View.VISIBLE);
+			
+			orderDeliveryDate = (TextView) row.findViewById(R.id.orderDeliveryDate);
+			orderDeliveryDate.setVisibility(View.VISIBLE);
+			orderDeliveryDate.setText(DateFormat.format("dd/MM/yyyy", order.getDateDelivery()));
+			
+			orderName.setText(order.getOrderName());
+			orderSupplier.setText(order.getSupplier().getCompanyName());
+			orderOpenDate.setText(DateFormat.format("dd/MM/yyyy", order.getDateOpen()));
+			orderCloseDate.setText(DateFormat.format("dd/MM/yyyy", order.getDateClose()));
+			
+			orderDetails.setOnClickListener(new View.OnClickListener() {
+				
+				@Override
+				public void onClick(View v) {
+					Intent intent = new Intent(getApplicationContext(), ShipOrderPurchasesActivity.class);
+					intent.putExtra("order", order);
+					startActivity(intent);
+				}
+			});
+			
+			return row;
+		}
+	}
+}
