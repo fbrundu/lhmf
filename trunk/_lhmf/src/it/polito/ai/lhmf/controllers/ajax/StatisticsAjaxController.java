e823ed8b6c14506763cd063b19d391570b175fcb
francesco.brundu@gmail.com <francesco.brundu@gmail.com@793d18dd-ad17-5a9a-07aa-97515492b075>
2012-10-05 22:55:31 +0000

    Refactoring

--- trunk/_lhmf/src/it/polito/ai/lhmf/controllers/ajax/StatisticsAjaxController.java
+++ trunk/_lhmf/src/it/polito/ai/lhmf/controllers/ajax/StatisticsAjaxController.java
@@ -5,22 +5,17 @@ import it.polito.ai.lhmf.model.MemberInterface;
 import it.polito.ai.lhmf.model.OrderInterface;
 import it.polito.ai.lhmf.model.ProductInterface;
 import it.polito.ai.lhmf.model.PurchaseInterface;
+import it.polito.ai.lhmf.model.StatisticsInterface;
 import it.polito.ai.lhmf.model.SupplierInterface;
 import it.polito.ai.lhmf.model.constants.MemberTypes;
 import it.polito.ai.lhmf.orm.Member;
-import it.polito.ai.lhmf.orm.Order;
 import it.polito.ai.lhmf.orm.Product;
-import it.polito.ai.lhmf.orm.Purchase;
-import it.polito.ai.lhmf.orm.PurchaseProduct;
 import it.polito.ai.lhmf.orm.Supplier;
 import it.polito.ai.lhmf.security.MyUserDetailsService;
 
 import java.util.ArrayList;
-import java.util.Calendar;
 import java.util.Iterator;
-import java.util.List;
 import java.util.Map;
-import java.util.Set;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
@@ -46,7 +41,8 @@ public class StatisticsAjaxController
 	private ProductInterface productInterface;
 	@Autowired
 	private PurchaseInterface purchaseInterface;
-	
+	@Autowired
+	private StatisticsInterface statisticsInterface;
 	
 	@PreAuthorize("hasRole('" + MyUserDetailsService.UserRoles.ADMIN + "')")
 	@RequestMapping(value = "/ajax/statMemberType", method = RequestMethod.POST)
@@ -100,65 +96,12 @@ public class StatisticsAjaxController
 	@PreAuthorize("hasRole('" + MyUserDetailsService.UserRoles.SUPPLIER + "')")
 	@RequestMapping(value = "/ajax/statSupplierMoneyMonth", method = RequestMethod.POST)
 	public @ResponseBody
-	ArrayList<Float> statSupplierMoneyMonth(HttpServletRequest request, HttpSession session,
-			@RequestParam(value = "year", required = true) int year) throws InvalidParametersException
+	ArrayList<Float> statSupplierMoneyMonth(HttpServletRequest request,
+			HttpSession session,
+			@RequestParam(value = "year", required = true) int year)
 	{
-		ArrayList<Float> respStat = new ArrayList<Float>();
-		
-		// Mi ricavo il membro Supplier
-		String username = (String) session.getAttribute("username");
-		Member memberSupplier = memberInterface.getMember(username);
-		
-		// Mi ricavo la lista degli ordini passati conclusi (con data consegna impostata)
-		List<Order> listOrders = orderInterface.getOldOrdersShippedBySupplier(memberSupplier, year);
-		
-		// Inizializzo variabili
-		Calendar cal = Calendar.getInstance();
-		Float price = new Float(0);
-		Set<Purchase> listPurchase;
-		
-		// Azzero struttura che servira per memorizzare gli incassi
-		for(int i = 0; i < 12; i++)
-			respStat.add(price);
-		
-		// Itero sugli ordini
-		for (Order temp : listOrders) {
-			
-			// Per ogni ordine estraggo il mese 
-			cal.setTime(temp.getDateClose());
-			int month = cal.get(Calendar.MONTH);
-			// Recupero il prezzo salvato
-			price = respStat.get(month);
-			//Azzero parziale
-			float partial = 0;
-			
-			//Accedo alla lista delle schede
-			listPurchase = temp.getPurchases();
-			
-			Set<PurchaseProduct> listPurchaseProduct;
-			
-			//Per ogni scheda d'acquisto mi ricavo il prodotto e relativa quantit�
-			for (Purchase tempPurchase : listPurchase) {
-				
-				listPurchaseProduct = tempPurchase.getPurchaseProducts();
-				Product tempProduct;
-				int tempAmount;
-				
-				for(PurchaseProduct tempPP : listPurchaseProduct) {
-					tempProduct = tempPP.getProduct();
-					tempAmount = tempPP.getAmount();
-					
-					partial += tempAmount*(tempProduct.getUnitCost());
-				
-				} // Fine prodotti di una scheda
-			} // Fine Schede di un ordine
-	
-			respStat.set(month, partial+price);
-			
-		} // fine Ordine di un responsabile
-		
-		
-		return respStat;
+		return statisticsInterface.getSupplierMoneyMonth(
+				(String) session.getAttribute("username"), year);
 	}
 	
 	@SuppressWarnings("rawtypes")
